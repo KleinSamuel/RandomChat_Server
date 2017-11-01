7c7f24a09914fcb1f03f01a84e5bd97ce8f7c553
sam <samuel.klein@online.de>
2017-11-01 17:15:36 +0100

    added main server script

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+var port = 9999;
+
+io.on('connection', function(socket){
+  console.log("a user connected");
+
+  socket.on('disconnect', function(){
+    console.log("Disconnected!");
+  });
+
+  socket.on('login', function(data){
+    console.log("Login data: "+data);
+    socket.emit('loginResponse', {'response':'OK'});
+  });
+
+});
+
+app.get('/hello', function(req, res){
+  console.log("hello world");
+});
+
+server.listen(port, function(){
+  console.log("RandomChat Server started on port "+port);
+});
